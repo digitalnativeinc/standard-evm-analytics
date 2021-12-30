c1a30bf97d002beb77ed324c2dbca4b3d4e80555
Hyungsuk Kang <hskang9@gmail.com>
2021-12-30 21:32:43 +0900

    Update constants.js

--- src/core/constants.js
+++ src/core/constants.js
@@ -3,6 +3,7 @@ export const TOKEN_DENY = [
   "0xc3761eb917cd790b30dad99f6cc5b4ff93c4f9ea",
   "0xe31debd7abff90b06bca21010dd860d8701fd901",
   "0xfc989fbb6b3024de5ca0144dc23c18a063942ac1",
+  "0x00C0e23d0205845378d079dcb8D5731383F1149b"
 ];
 
 export const PAIR_DENY = ["0xb6a741f37d6e455ebcc9f17e2c16d0586c3f57a5"];
@@ -43,7 +44,7 @@ export const SCANNERS = {
     },
   },
   [ChainId.SHIDEN]: {
-    name: "Subscan",
+    name: "Blockscout",
     url: "https://blockscout.com/shiden",
     getUrl: (id) => {
       return `https://blockscout.com/shiden/account/${id}`;
